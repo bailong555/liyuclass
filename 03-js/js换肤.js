f8389e8e0f16b506c7207d5d5f1214e6cfe8a0a9
Mr bai <787205919@qq.com>
2019-04-26 15:42:57 +0800

    2.html

---
+++ 03-js/js换肤.js
@@ -0,0 +1,15 @@
+function toImg(){
+	document.getElementById('TP').href="../02-css/s.css"
+}
+function toImages(){
+	document.getElementById('TP').href="../02-css/wz.css"
+}
+
+
+var oSsm=document.getElementById('ssm');
+oSsm.onclick = toImg;
+var oWz=document.getElementById('wz');
+oWz.onclick = toImages;
+
+// onclick='toImg()' 
+// onclick='toImages()' 
