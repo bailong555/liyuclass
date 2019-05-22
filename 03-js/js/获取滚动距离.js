15212ca6359526cefd348f150b2b16992b602afe
Mr bai <787205919@qq.com>
2019-05-22 10:30:11 +0800

    5.22

---
+++ 03-js/js/获取滚动距离.js
@@ -0,0 +1,6 @@
+function getScrollTop(){
+	return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+}
+function getScrollLeft(){
+	return window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft;
+}
