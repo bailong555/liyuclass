ebf9e3088f1927a8d21f9c652efaa92fea16c1cd
bai <787205919@qq.com>
2019-07-17 08:55:37 +0800

    1111

---
+++ 02-css/js/03-适配.js
@@ -0,0 +1,11 @@
+(function(win,doc){
+	var Root = document.getElementsByTagName('document');
+
+	var width = window.document.clientWidth || window.body.clientWidth;
+
+
+
+
+
+
+})(window,document)   
